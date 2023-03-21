c85c42aa9b49fffe071698fce7be5ef46e738026
DhanikaSankar <89347760+DhanikaSankar@users.noreply.github.com>
2023-03-21 14:30:17 +0530

    Delete Data.jsx

--- src/Data.jsx
+++
@@ -1,5 +0,0 @@
-export const data=[{name:"Adventures of Tom Sawyer",author:" Mark Twaiw",pages:"425",image:"https://www.wallpapers13.com/wp-content/uploads/2016/01/Cool-and-Beautiful-Nature-desktop-wallpaper-image-2560X1600-1600x1200.jpg"},
-{name:"Agni Veena",author:"Kazi Nasrul Islam",pages:"300",image:"https://images.designtrends.com/wp-content/uploads/2016/04/02133843/Beautiful-Horizon-Background.jpg"},
-{name:"Animal Farm",author:"George Orwell",pages:"350",image:"https://www.wallpapers13.com/wp-content/uploads/2016/01/Cool-and-Beautiful-Nature-desktop-wallpaper-image-2560X1600-1600x1200.jpg"},
-{name:"Ben Hur",author:"Lewis Wallace",pages:"100",image:"https://images.designtrends.com/wp-content/uploads/2016/04/02133843/Beautiful-Horizon-Background.jpg"},
-{name:"Baburnama",author:"Babur",pages:"120",image:"https://www.wallpapers13.com/wp-content/uploads/2016/01/Cool-and-Beautiful-Nature-desktop-wallpaper-image-2560X1600-1600x1200.jpg"}]
